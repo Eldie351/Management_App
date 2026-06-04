6734e480cd8067cd35d0787aebad5559ff594553
Inarie351 <kim.degnon@epitech.eu>
2026-06-05 00:55:18 +0100

    [Feat]: add schema prisma(set database)

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from '../prisma/prisma.module';
+//import * as bcrypt from 'bcrypt'; 
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async register(email: string, name: string, passwordPlain: string) {
+    const existingUser: User | null = await this.prisma.user.findUnique({ where: { email } });
+    if (existingUser) {
+      throw new Error('Email already in use');
+    }
+  }
+}
